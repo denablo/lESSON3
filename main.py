592fc84acab6151ff5c731c246d6837c6740c2ec
denablo <deniskrytoy3@gmail.com>
2022-10-29 10:15:41 +0300

    first comit

---
+++ main.py
@@ -0,0 +1,9 @@
+import random
+class Human:
+    def __init__(self, name = "Human", job = None, home = None):
+        self.name = name
+        self.job = job
+        self.money = 100
+        self.gladness = 50
+        self.sateity = 50
+

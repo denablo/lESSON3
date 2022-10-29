b08373c7f4e497b7da60824921ec8588f2635949
denablo <deniskrytoy3@gmail.com>
2022-10-29 11:15:35 +0300

    first comit

--- main.py
+++ main.py
@@ -6,4 +6,20 @@ class Human:
         self.money = 100
         self.gladness = 50
         self.sateity = 50
+        def get_home(self):
+            self.home = House()
+        def get_food(self):
+            if self.home.food <= 0:
+                self.shopping = 'food'
+            elif self.satiety >= 100
+                self.satiety = 100
+                return
+            self.satiety += 5
+            self.home.food -= 5
+
+class House:
+    def__unit__(self):
+           self.mess = 0
+           self.food = 0
+           self.ecological = 0
 

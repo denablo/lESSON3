ad51348a2a8eb2cbb43131bd50cc05c759628e81
denablo <deniskrytoy3@gmail.com>
2022-10-29 11:47:34 +0300

    first comit

--- main.py
+++ main.py
@@ -11,15 +11,37 @@ class Human:
         def get_food(self):
             if self.home.food <= 0:
                 self.shopping = 'food'
-            elif self.satiety >= 100
+            elif self.satiety >= 100:
                 self.satiety = 100
                 return
             self.satiety += 5
             self.home.food -= 5
+        def get_bycicle = Bucicle(color_of_bycicle)
+        def get_home(self):
+            self.home = House()
+
+class Bycicle:
+    def __init__(self, color_of_bycicle):
+        self.color_of_bycicle = ["red", 'green', 'blue']
+        if choce(self.color_of_bycicle) == 'red':
+            self.color_of_bycicle = 'red'
+        else choce(self.color_of_bycicle) == 'green':
+            self.color_of_bycicle = 'green'
+        else:
+            self.color_of_bycicle = 'blue'
+
+
+
+
+
+
+
+
+
 
 class House:
-    def__unit__(self):
-           self.mess = 0
-           self.food = 0
-           self.ecological = 0
+    def __init__(self):
+        self.mess = 0
+        self.food = 0
+        self.ecological = 0
 
